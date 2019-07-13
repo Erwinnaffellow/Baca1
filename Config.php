8b80d53390c4d8d4c90b193fb2746ec4b6e82307
Erwinnaffellow <52817160+Erwinnaffellow@users.noreply.github.com>
2019-07-13 05:29:32 +0000

    Create Config.php

---
+++ Config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$uid="Facebook_1340697149426831"
+$raw_uid="bdca39cb-f05b-496b-b3a4-220a606377d1"
+$user_name="Erwinnaffellow"
+$invite_code="F68hixUaGV"
+$user_token="Abyhk5T_tRw1fz9R"
+$own_mac="08%3A8C%3A2C%3A05%3A18%3ADD"
+$android_id="718d9b0ae4046d03"
+$device_type="samsung++SM-J105F"
+
+?>
